ef62e4ccd3c298b57a9fe83f25745d754d24c09c
Skey1 <66116292+Skey1@users.noreply.github.com>
2022-10-26 09:07:54 +0330

    Add files via upload
    lesson 1-6 Laravel-a

---
+++ web.php
@@ -0,0 +1,44 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// Route::get('/users', function () {
+//     return "hi";
+// });
+
+// Route::get('/{id}', function($id) {
+//     return $id;
+// });
+
+// Route::get('/{id?}', function($id=1) {
+//     return $id;
+// });
+
+// Route::get('/{id}', function($id) {
+//     return $id;
+// })->where('id', '[0-9]+');
+
+// Route::get('/{username}', function($username) {
+//     return $username;
+// })->where('username', '[A-Za-z]+');
+
+// Route::get('/members/{id}', 'MembersController@show')->name('members.show');
+
